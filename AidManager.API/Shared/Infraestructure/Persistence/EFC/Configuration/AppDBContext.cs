d22e85ec998822642f6f3fda42304b19dbf45536
Arian Rodriguez <arianmrv12@gmail.com>
2024-05-26 18:21:45 -0500

    add: table to db

--- AidManager.API/Shared/Infraestructure/Persistence/EFC/Configuration/AppDBContext.cs
+++ AidManager.API/Shared/Infraestructure/Persistence/EFC/Configuration/AppDBContext.cs
@@ -1,4 +1,5 @@
-﻿using AidManager.API.Shared.Infraestructure.Persistence.EFC.Configuration.Extensions;
+﻿using AidManager.API.SampleBounded.Domain.Model.Aggregates;
+using AidManager.API.Shared.Infraestructure.Persistence.EFC.Configuration.Extensions;
 using EntityFrameworkCore.CreatedUpdatedDate.Extensions;
 using Microsoft.EntityFrameworkCore;
 
@@ -18,6 +19,11 @@ public class AppDBContext : DbContext
     {
         // this method is to configure the database schema and the tables
         base.OnModelCreating(builder);
+        
+        // here we can configure the tables
+        builder.Entity<Book>().ToTable("Books");
+        builder.Entity<Book>().HasKey(b => b.Id);
+        builder.Entity<Book>().Property(b => b.Id).IsRequired().ValueGeneratedOnAdd();
         
         // this is to the name tables conver to snake case "LuchoPortuano" -> "lucho_portuano"
         builder.UseSnakeCaseNamingConvention();
