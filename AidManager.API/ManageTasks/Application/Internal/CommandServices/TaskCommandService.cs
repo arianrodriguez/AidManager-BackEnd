7d1d3016260d1ca7a55fb34a1f343aa77bc2a9bc
SEBRH <u202211894@upc.edu.pe>
2024-06-04 17:00:54 -0500

    feat(manageTasks): Application implemented

---
+++ AidManager.API/ManageTasks/Application/Internal/CommandServices/TaskCommandService.cs
@@ -0,0 +1,49 @@
+﻿using AidManager.API.ManageTasks.Domain.Model.Aggregates;
+using AidManager.API.ManageTasks.Domain.Model.Commands;
+using AidManager.API.ManageTasks.Domain.Repositories;
+using AidManager.API.ManageTasks.Domain.Services;
+using AidManager.API.Shared.Domain.Repositories;
+
+namespace AidManager.API.ManageTasks.Application.Internal.CommandServices;
+
+public class TaskCommandService(ITaskRepository taskRepository, IUnitOfWork unitOfWork) : ITaskCommandService
+{
+    public async Task<TaskItem> Handle(CreateTaskCommand command)
+    {
+        
+        Console.WriteLine("command service called");
+        var task = new TaskItem(command);
+        Console.WriteLine("task Aggregate created");
+        await taskRepository.CreateTaskItem(task);
+        Console.WriteLine("task added to repository");
+        await unitOfWork.CompleteAsync();
+        Console.WriteLine("unit of work completed");
+        return task;
+    }
+    
+    public async Task<TaskItem> Handle(UpdateTaskCommand command)
+    {
+
+        var task = await taskRepository.FindByIdAsync(command.Id);
+        Console.WriteLine("command service called");
+        if (task is null) throw new Exception("Task not found");
+        task.UpdateTask(command);
+        Console.WriteLine("task Aggregate updated");
+        await taskRepository.Update(task);
+        Console.WriteLine("task updated in repository");
+        await unitOfWork.CompleteAsync();
+        Console.WriteLine("unit of work completed");
+        return task;
+    }
+    
+    public async Task<TaskItem> Handle(DeleteTaskCommand command)
+    {
+        var task = await taskRepository.FindByIdAsync(command.Id);
+        if (task is null) throw new Exception("Task not found");
+        await taskRepository.Remove(task);
+        await unitOfWork.CompleteAsync();
+        return task;
+    }
+    
+    
+}
---
+++ AidManager.API/ManageTasks/Application/Internal/QueryServices/TaskQueryService.cs
@@ -0,0 +1,19 @@
+﻿using AidManager.API.ManageTasks.Domain.Model.Aggregates;
+using AidManager.API.ManageTasks.Domain.Model.Queries;
+using AidManager.API.ManageTasks.Domain.Repositories;
+using AidManager.API.ManageTasks.Domain.Services;
+
+namespace AidManager.API.ManageTasks.Application.Internal.QueryServices;
+
+public class TaskQueryService(ITaskRepository taskRepository) : ITaskQueryService
+{
+    public async Task<IEnumerable<TaskItem>> Handle(GetAllTasksQuery query)
+    {
+        return await taskRepository.ListAsync();
+    }
+        
+    public async Task<TaskItem> Handle(GetTaskByIdQuery query)
+    {
+        return await taskRepository.FindByIdAsync(query.Id);
+    }
+}
