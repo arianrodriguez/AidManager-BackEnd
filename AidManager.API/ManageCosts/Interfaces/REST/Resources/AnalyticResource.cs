65e0d7d94e7dc700755b7d4e04af69685b712524
U202210138 <129425974+U202210138@users.noreply.github.com>
2024-06-04 15:49:04 -0500

    feat(analytics): added Analytic Resources, deleted ola.txt

---
+++ AidManager.API/ManageCosts/Interfaces/REST/Resources/AnalyticResource.cs
@@ -0,0 +1,3 @@
+﻿namespace AidManager.API.ManageCosts.Interfaces.REST.Resources;
+
+public record AnalyticResource(int Id, string Title, string Description, double Cost, double Progress,int[] Current, int[] Expected);
