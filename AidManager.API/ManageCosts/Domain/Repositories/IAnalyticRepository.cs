ae161b563f7869c6f8d7532f69361841942d82ad
U202210138 <129425974+U202210138@users.noreply.github.com>
2024-06-04 15:48:41 -0500

    feat(analytics): added IAnalytic Repository, deleted ola.txt

---
+++ AidManager.API/ManageCosts/Domain/Repositories/IAnalyticRepository.cs
@@ -0,0 +1,8 @@
+﻿using AidManager.API.ManageCosts.Domain.Model.Aggregates;
+using AidManager.API.Shared.Domain.Repositories;
+namespace AidManager.API.ManageCosts.Domain.Repositories;
+
+public interface IAnalyticRepository : IBaseRepository<Analytic>
+{
+    public Task<Analytic> CreateAnalytic(Analytic entity);
+}
