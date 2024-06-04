f514b88498c6716eb68e07123d8d3ee4159a5a12
U202210138 <129425974+U202210138@users.noreply.github.com>
2024-06-04 15:48:22 -0500

    feat(analytics): added Analytic Command Service and Analytic Query Service, deleted ola.txt

---
+++ AidManager.API/ManageCosts/Domain/Services/IAnalyticQueryService.cs
@@ -0,0 +1,9 @@
+﻿using AidManager.API.ManageCosts.Domain.Model.Aggregates;
+using AidManager.API.ManageCosts.Domain.Model.Queries;
+
+namespace AidManager.API.ManageCosts.Domain.Services;
+
+public interface IAnalyticQueryService
+{
+    Task<Analytic> Handle(GetAnalyticByIdQuery query);
+}
